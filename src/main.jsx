92faf5a7442586ed579495e9bd8336eebd21c4d3
lajkogege <lajkogege@gmail.com>
2025-01-07 10:35:40 +0100

    körbe ölelés a mainbe

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FileProvider } from './context/FileContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-  
+    <FileProvider>
+      <App />
+    </FileProvider>
+  </StrictMode>
+
 )
